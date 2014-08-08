7da0e08cc9eb00516f86709581fbb5b7927864f0
Bulux <Bulux@Bulux-PC>
2014-08-08 07:14:37 -0600

    stack

--- src/hdt4/StackList.java
+++ src/hdt4/StackList.java
@@ -16,25 +16,4 @@ public class StackList<E> extends AbstractStack<E> {
  public E pop(){return data.remove(size()-1);}      
  public E peek(){return data.get(size() - 1);}
  public int size(){return data.size();}
- 
 }
-
-
-/*
-public class StackList<E> extends AbstractStack<E> {
-    protected List<E> data;
-
-      ---------------    Esto ya es para el factory --------------------- 
-public StacktList(){
-       ListFactory<E> factory = new ListFactory<E>();
-       data = factory.getList();
-   }
-    
-   
-    
- public void push(E item){data.addLast(item);}
- public E pop(){return data.removeFirst();}      
- public E peek(){return data.getFirst();}
- public int size(){return data.size();}
-
-*/
